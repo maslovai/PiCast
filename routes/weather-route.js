415c53b717324dc3ff998f379fbbedea9b148525
Iryna Maslova <maslovai@hotmail.com>
2017-11-17 14:13:16 -0800

    set up record schema and mlab collection

--- routes/weather-route.js
+++ routes/weather-route.js
@@ -1,10 +1,22 @@
 'use strict';
+require('dotenv').config();
+const Record = require('../model/record');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const MONGODB_URI = process.env.MONGODB_URI||'mongodb://maslovai:Anna2009@ds119585.mlab.com:19585/forecast'
+mongoose.connect(MONGODB_URI);
+const superagent = require('superagent');
+// const jsonParser= require('body-parser');
+// require('../lib/server');
 
-const apiKey = process.env.API_KEY;
 const router = module.exports = require('express').Router();
 
-
-router.get(`http://api.wunderground.com/api/${apiKey}/conditions/q/WA/Seattle.json`, (req, res, next)=>{
-    res.send(req.body);
-    next();
+superagent.get(`http://api.wunderground.com/api/${process.env.API_KEY}/conditions/q/WA/Seattle.json`)
+.then(req=>{
+    require('body-parser');
+    let record = new Record({"city":req.body.current_observation.display_location.full, "temperature":req.body.current_observation.temp_f, "percip":req.body.current_observation.icon});
+    record.save()
+    .then(console.log(req.body))
+    .catch(err=>console.log(err))
 })
+.catch(err=>console.log(err))
