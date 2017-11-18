c734a7ce043d7aa96993f2c921fa1d4497b9abf3
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-18 11:36:22 -0800

    rewrote request as callback

--- model/record.js
+++ model/record.js
@@ -2,9 +2,9 @@
 const mongoose = require('mongoose');
 
 const record = new mongoose.Schema({
-    city:{type:String, required:true},
-    temperature:{type: String, required:true},
-    percip:{type:String, required: true}
-})
+  city:{type:String, required:true},
+  temperature:{type: String, required:true},
+  percip:{type:String, required: true},
+});
 
 module.exports = mongoose.model('Record', record);
