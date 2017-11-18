d0f28d8bd9febc67a88dba35f4ae10c41eea53d7
Iryna Maslova <maslovai@hotmail.com>
2017-11-18 10:33:37 -0800

    before pull

--- routes/weather-route.js
+++ routes/weather-route.js
@@ -6,14 +6,12 @@ mongoose.Promise = require('bluebird');
 const MONGODB_URI = process.env.MONGODB_URI||'mongodb://maslovai:Anna2009@ds119585.mlab.com:19585/forecast'
 mongoose.connect(MONGODB_URI);
 const superagent = require('superagent');
-// const jsonParser= require('body-parser');
-// require('../lib/server');
+
 
 const router = module.exports = require('express').Router();
 
 superagent.get(`http://api.wunderground.com/api/${process.env.API_KEY}/conditions/q/WA/Seattle.json`)
 .then(req=>{
-    require('body-parser');
     let record = new Record({"city":req.body.current_observation.display_location.full, "temperature":req.body.current_observation.temp_f, "percip":req.body.current_observation.icon});
     record.save()
     .then(console.log(req.body))
