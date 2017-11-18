581447780b1c6fd395f7eca3d66e8dd0e83b179e
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-18 11:48:37 -0800

    added date

--- model/record.js
+++ model/record.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 
 const record = new mongoose.Schema({
+  date: {type: Date, default: () => new Date()},
   city: {type:String, required:true},
   temperature: {type: String, required:true},
   forecast: {type:String, required: true},
