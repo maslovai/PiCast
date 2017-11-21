f6997db189c04f08651a7c8468e98d4b0cad7b56
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-20 19:13:40 -0800

    moved stuff to routes folder

--- weather-index.js
+++ weather-index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const cron = require('node-cron');
-const getWeather = require('./lib/get-weather.js');
+const getWeather = require('./routes/get-weather.js');
 
 cron.schedule('0 0-23 * * *', getWeather);
 
