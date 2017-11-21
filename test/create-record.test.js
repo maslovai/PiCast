85b86bcc70c3cf220ea8ae2ce22ed6b4346834ab
Iryna Maslova <maslovai@hotmail.com>
2017-11-20 20:14:35 -0800

    committing to merge

---
+++ test/create-record.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Record = require('../model/record');
+const create = require('../model/create-record');
+
+describe('create-record should create a record and save it to the database', () => {
+
+})
