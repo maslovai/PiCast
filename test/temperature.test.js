592b2bcc195a1d044671555fa00b160591b46873
Kyle Richey <richey.kyle.i@gmail.com>
2017-11-21 09:14:45 -0800

    starting to run tests, running into 'module did not self-register' err

--- test/temperature.test.js
+++ test/temperature.test.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const expect = require('expect');
+const temp = require('../lib/light-display/temperature.js');
+const Gpio = require('onoff').Gpio;
+
+describe('Temperature module', function(){
+
+  it('should light up all lights when 90 or above', function() {
+
+    let temp = '90';
+
+    expect();
+
+  });
+
+});
