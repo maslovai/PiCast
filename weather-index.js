46f547ca47e4fb199d934fc0173affeac238fd46
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-20 19:03:31 -0800

    fixed

--- weather-index.js
+++ weather-index.js
@@ -3,6 +3,5 @@
 const cron = require('node-cron');
 const getWeather = require('./lib/get-weather.js');
 
-// cron.schedule('20 0-23 * * *', getWeather);
+cron.schedule('20 0-23 * * *', getWeather);
 
-cron.schedule('*/10 * * * * *', getWeather);
