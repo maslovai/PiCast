effe6af29dc5c6e156e6746c74a3cb5b3a09e793
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-20 16:00:00 -0800

    restructured file system

--- weather-index.js
+++ weather-index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const cron = require('node-cron');
-const getWeather = require('./get-weather.js');
+const getWeather = require('./lib/get-weather.js');
 
 cron.schedule('20 0-23 * * *', getWeather);
