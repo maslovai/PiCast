3ef49b1ce04676d5e207f2f4d458dfd13e62bc31
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-22 07:04:38 -0800

    Update get-weather.js

--- routes/get-weather.js
+++ routes/get-weather.js
@@ -5,8 +5,8 @@ const getLocation = require('./locate.js');
 const createRecord  = require('../model/create-record.js');
 const saveRecord = require('../model/save-record.js');
 
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+// const mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');
 
 // weather request
 let getWeather = module.exports = () => {
@@ -16,4 +16,4 @@ let getWeather = module.exports = () => {
     .catch(err => console.log(err.message));
 };
 
-getWeather();
+// getWeather();
