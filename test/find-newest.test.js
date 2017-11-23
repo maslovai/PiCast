36c5b7bd1c6ccca64602efa05d4fa7a38b1429d5
Iryna Maslova <maslovai@hotmail.com>
2017-11-22 17:47:16 -0800

    moved clear db in to a separate module

--- test/find-newest.test.js
+++ test/find-newest.test.js
@@ -2,13 +2,14 @@
 const create = require('../model/create-record');
 const newest = require('../lib/find-newest')
 const expect = require('expect');
+const superagent = require('superagent');
 
 const mockData = {
     req : {
          body : {
              current_observation : {
                 display_location : {
-                  full : "Kiev"
+                  full : "testCity"
                   },
                 temp_f : "near zero",
                 icon : "almost always snow"
@@ -25,8 +26,14 @@ it('should return the latest record from the databaseand remove all but last rec
     .then(() => {
         newest()
         .then(record=>{
-            expect(record.city).toBe("Kiev");
+            expect(record.city).toBe("testCity");
             expect(record.forecast).toBe("almost always snow");
+            superagent.get(`https://api.mlab.com/api/1/databases/forecast/collections/records?q={"active": true}&c=true&apiKey=${process.env.MLAB_KEY}`)
+            .then(res=>{
+                console.log("test after request count to db: ",res);
+                expect (res).toEqual(1);
+            })
+            .catch(err=>console.log(err))
         })
         .catch(err=>console.log(err))
     })
