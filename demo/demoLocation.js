ba61148b1ff0960a123a583b213565e51432c783
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-22 19:46:17 -0800

    demo object ready for hardware portion

---
+++ demo/demoLocation.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const createRecord = require('../model/create-record.js');
+const Record = require('../model/record.js');
+
+const location = {
+  city: process.argv[2],
+  state: process.argv[3],
+};
+
+createRecord(location)
+  .then(data => {
+    let record = new Record({
+      'city': data.city,
+      'state': data.state,
+      'temperature': data.temperature,
+      'forecast': data.forecast,
+      'alert': data.alert,
+    });
+    return record;
+  })
+  .then(console.log)
+  .catch(console.log);
+
