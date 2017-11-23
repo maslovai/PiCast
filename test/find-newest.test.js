c4299f9e06301c06620b98df563b6ec05008abf6
Iryna Maslova <maslovai@hotmail.com>
2017-11-22 21:07:13 -0800

    pulling from master

--- test/find-newest.test.js
+++ test/find-newest.test.js
@@ -25,22 +25,16 @@ afterAll(()=>{
 
 describe('find-newest', ()=>{
 
-it('should return the latest record from the databaseand and remove all but last record from the databse', ()=>{
-    create(mockData.req)
-    .then(() => {
-        newest()
-        .then(record=>{
-            expect(record.city).toBe("testCity");
-            expect(record.forecast).toBe("snow");
-            superagent.get(`https://api.mlab.com/api/1/databases/forecast/collections/records?q={"active": true}&c=true&apiKey=nyR452n-EvkXAry0rC2ntLtG1OA38N3z`)
-            .then(count=>{
-                console.log("test after request count to db: ", count);
-                expect(count).toEqual(1);
-            })
-            .catch(err=>console.log(err))
-        })
+    it('should return the latest record from the database', ()=>{
+         create(mockData.req)
+             .then(() => {
+              newest()
+             .then(record=>{
+                 expect(record.city).toBe("testCity");
+                 expect(record.forecast).toBe("snow");
+             })
         .catch(err=>console.log(err))
     })
-    
 })
+
 })
