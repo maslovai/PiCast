6a9727f760ebb358f820d85a33c53d1289f074c5
Kyle Richey <richey.kyle.i@gmail.com>
2017-11-25 17:38:21 -0800

    forecast test 100% done

--- test/lights/forecast.test.js
+++ test/lights/forecast.test.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 const sinon = require('sinon');
-const forecast = require('../lib/mocks/mockforecast.js');
+const forecast = require('/Users/kylerichey/codefellows/401/midterm-project/PiCast/lib/mocks/mockforecast.js');
 
 describe('solid lights', function() {
 
@@ -49,21 +49,63 @@ describe('blinking lights', function() {
     spy.restore();
 
   });
-
 });
 
 describe('severe weather warning', function() {
 
-  it('should turn on red light for severe weather and white for snow', function() {
+  it('should turn on red light for severe weather and blue for sleet', function() {
 
     let spy = sinon.spy(console, 'log');
     forecast('sleet');
     assert(spy.calledWith('21: 1'));
-    assert(spy.calledWith('23: 1'));
-    assert(spy.calledWith('blinking stopped'));
-    assert(spy.calledWith('1 = 0'));
-    assert(spy.calledWith('0 = 0'));
+    assert(spy.calledWith('18: 1'));
     spy.restore();
   });
 
+  it('should turn on red and blue light', function() {
+
+    let spy = sinon.spy(console, 'log');
+    forecast('tstorms');
+    assert(spy.calledWith('18: 1'));
+    assert(spy.calledWith('21: 1'));
+    spy.restore();
+
+  });
+});
+
+describe('weather alerts', function() {
+
+  it('should log 21 OFF if count is 0', function(){
+
+    let spy = sinon.spy(console, 'log');
+    forecast('snow', 'WIN');
+    assert(spy.calledWith('21: off'));
+    assert(spy.calledWith('blinking'));
+    spy.restore();
+
+  });
+
+  it('should turn on blue alert', function(){
+
+    let spy = sinon.spy(console, 'log');
+    forecast('sleet', 'FLO');
+    assert(spy.calledWith('21: off'));
+    assert(spy.calledWith('18: 1'));
+    assert(spy.calledWith('blinking stopped'));
+    assert(spy.calledWith('0 = 0'));
+    spy.restore();
+
+  });
+
+  it('should turn on yellow alert', function(){
+
+    let spy = sinon.spy(console, 'log');
+    forecast('sunny', 'HEA');
+    assert(spy.calledWith('21: off'));
+    assert(spy.calledWith('24: 1'));
+    assert(spy.calledWith('blinking stopped'));
+    assert(spy.calledWith('0 = 0'));
+    spy.restore();
+
+  });
 });
